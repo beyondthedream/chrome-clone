a54051c96ede198175e639237502816abcc03683
Yena Kwon <ynkwon@softrain.co.kr>
2022-08-01 03:23:32 +0900

    init

--- js/background.js
+++ js/background.js
@@ -5,7 +5,6 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
-// bgImage.style.height = "100vh";
 bgImage.style.backgroundSize = "cover";
 
 bgImage.src = `img/${chosenImage}`;
